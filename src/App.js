7854f5b35c083213c71f57dba2bff650f612fd36
jefishub <jere.manni@gmail.com>
2021-07-31 15:29:24 +0300

    initial commit with copied files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CategoryBox from './CategoryBox';
+import './css/styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>My homepage</p>
+      <div className="category-section"> 
+        <CategoryBox />
+        <CategoryBox />
+        <CategoryBox />
+      </div>
     </div>
   );
 }
---
+++ src/CategoryBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function CategoryBox() {
+
+    return (
+        <div className="category-box">
+            <h1>
+                Test
+            </h1>
+        </div>
+    )
+}
+
+export default CategoryBox;
