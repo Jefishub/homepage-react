71d03072a11d30c5470b000da8a7e4809adf8d16
jefishub <jere.manni@gmail.com>
2021-08-01 13:42:34 +0300

    added educationpage content

--- src/components/banner/mainBanner.js
+++ src/components/banner/mainBanner.js
@@ -11,11 +11,12 @@ function CategoryBox() {
             <div className="hero-image">
                 <div className="hero-text">
                     <h1 className="banner-header"><u>Jere Manni</u></h1>
-                    <p>Master of <span style={banner_text_style}>Physics</span></p>
+                    <p><span style={banner_text_style}>Master</span> of Physics</p>
+                    <p>Full-stack<span style={banner_text_style}> Developer</span></p>
                     <p><span style={banner_text_style}>Expert</span> in Materials Science</p>
-                    <p>10 years working <span style={banner_text_style}>experience</span></p>
-                    <p><span style={banner_text_style}>Python</span>-programmer</p>
-                    <p>Future <span style={banner_text_style}>IT specialist</span></p>
+                    <p>10+ years of working <span style={banner_text_style}>Experience</span></p>
+                    <p>2 years of <span style={banner_text_style}>Team Lead</span> experience</p>
+
                 </div>
             </div>
         </div>
