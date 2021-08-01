11a5f4cd31ad76fb50ca6ce30a481452ffa93dc9
jefishub <jere.manni@gmail.com>
2021-08-01 15:11:24 +0300

    using now new bulletList component, changed content naming

--- src/pages/educationpage/content.js
+++ src/pages/educationpage/content.js
@@ -1,46 +1,52 @@
-const school = [
-    {header: "Haaga-Helia University of Applied Sciences (ONGOING)",
-     bullets: [
-        "Bachelor of Business Information Technology (2020 -> )",
-        "Software Production"
-    ]},
-    {header: "Taitotalo (2020 - 2021)",
-    bullets: [
-       "Vocational Qualification in Information Technology",
-       "Python-programmer"
-    ]},
-    {header: "University of Turku (2005 - 2012)",
-    bullets: [
-        "Master of Science (physics)",
-        "Main: Materials Science",
-        "Minor: Mathemathics, Chemisty, IT"
-    ]},
-    {header: "Kastu High School, Turku (2002 - 2005)",
-    bullets: [
-        "Graduate",
-        "Natural Sciences major"
+const school = {
+    header: "Education",
+    content: [
+        {name: "Haaga-Helia University of Applied Sciences (ONGOING)",
+        bullets: [
+            "Bachelor of Business Information Technology (2020 -> )",
+            "Software Production"
+        ]},
+        {name: "Taitotalo (2020 - 2021)",
+        bullets: [
+        "Vocational Qualification in Information Technology",
+        "Python-programmer"
+        ]},
+        {name: "University of Turku (2005 - 2012)",
+        bullets: [
+            "Master of Science (physics)",
+            "Main: Materials Science",
+            "Minor: Mathemathics, Chemisty, IT"
+        ]},
+        {name: "Kastu High School, Turku (2002 - 2005)",
+        bullets: [
+            "Graduate",
+            "Natural Sciences major"
+        ]}
+]}
+
+const coursesAndTraining = {
+    header: "Courses and Training",
+    content: [{
+        name: "Courses and training",
+        bullets: [
+            "Complete Python 3 Bootcamp (Udemy, 2020)",
+            "Physical Vapor Deposition and Thin Film Growth Models (2018)",
+            "Introduction to Atomic Layer Deposition (2018)",
+            "Introduction to Digital Marketing (2018)",
+            "Unwelcome organisms (mold) living in our properties (2018)",
+            "Course to certificate for Asbestos and Harmful Materials Expert (2017)"
     ]}
-]
+]}
 
-const coursesAndTraining = [{
-    header: "Courses and training",
-    bullets: [
-        "Complete Python 3 Bootcamp (Udemy, 2020)",
-        "Physical Vapor Deposition and Thin Film Growth Models (2018)",
-        "Introduction to Atomic Layer Deposition (2018)",
-        "Introduction to Digital Marketing (2018)",
-        "Unwelcome organisms (mold) living in our properties (2018)",
-        "Course to certificate for Asbestos and Harmful Materials Expert (2017)"
-    
-]}]
-
-const certificates = [{
+const certificates = {
     header: "Certificates",
-    bullets: [
-        "Laser Safety Training (2019)",
-        "First Aid Course -EA1 (2018 - 2021)",
-        "Profiency Certificate P401 - Identification of Asbestos in Bulk Materials (PLM)",
-    ]
-}]
+    content: [{
+        name: "Certificates",
+        bullets: [
+            "Laser Safety Training (2019)",
+            "First Aid Course -EA1 (2018 - 2021)",
+            "Profiency Certificate P401 - Identification of Asbestos in Bulk Materials (PLM)",
+        ]}
+]}
 
 export { school, coursesAndTraining, certificates }
--- src/pages/educationpage/index.js
+++ src/pages/educationpage/index.js
@@ -2,34 +2,17 @@ import React from 'react';
 
 import '../../css/styles.css';
 
+import bulletList from '../../components/bulletList'
+
 import { school, coursesAndTraining, certificates } from './content'
 
 function Education() {
 
-  const education = (content) => {
-    const listItems = content.map((item, i) => {
-      const header = <h2>{item.header}</h2>
-      const bullets = item.bullets.map((bullet, i) => {
-        return <p key={i}>{bullet}</p>
-    })
-      return (
-        <div className="text_box" key={i}>
-          {header}
-          {bullets}
-        </div>
-      )
-    })
-    return listItems
-  }
-
   return (
     <div className="bordered_box">
-      <div className="text_box">
-          <h1>Education</h1>
-      </div>
-      {education(school)}
-      {education(coursesAndTraining)}
-      {education(certificates)}
+      {bulletList(school)}
+      {bulletList(coursesAndTraining)}
+      {bulletList(certificates)}
     </div>
   );
 }
