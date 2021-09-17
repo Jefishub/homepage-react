360d1f895d4393ef86cfbc6ba79032c038112195
jefishub <jere.manni@gmail.com>
2021-09-17 23:54:26 +0300

    fixed some styling issues

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,8 +7,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 function Footer() {
 
     return (
-        <Navbar bg="dark" variant="dark">
-            <p style={{ color: "white", margin: "0px 0px 8px 16px" }}>
+        <Navbar bg="dark" variant="dark" style={{position: "relative", bottom: "0", width: "100%"}}>
+            <p style={{ color: "white", margin: "0px 0px 0px 16px" }}>
                 Author: Jere Manni <br></br>
                 Created with <a href="https://github.com/Jefishub/homepage-react" style={{ color: "yellow" }}>React</a>, Deployed with <a href="https://firebase.google.com/" style={{ color: "yellow" }}>Firebase</a>
             </p>
