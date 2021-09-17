f9e9ab2e8ab62a6a922799b938d5d20ec70e72a2
jefishub <jere.manni@gmail.com>
2021-09-17 23:15:37 +0300

    changed footer to navbar component, changed colors

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,15 +1,18 @@
 
 import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 function Footer() {
 
     return (
-        <footer>
-            <p>
+        <Navbar bg="dark" variant="dark">
+            <p style={{ color: "white", margin: "0px 0px 8px 16px" }}>
                 Author: Jere Manni <br></br>
-                Created with <a href="https://github.com/Jefishub/homepage-react" style={{color: "yellow"}}>React</a>, Deployed with <a href="https://firebase.google.com/" style={{color: "yellow"}}>Firebase</a>
+                Created with <a href="https://github.com/Jefishub/homepage-react" style={{ color: "yellow" }}>React</a>, Deployed with <a href="https://firebase.google.com/" style={{ color: "yellow" }}>Firebase</a>
             </p>
-        </footer>
+        </Navbar>
     )
 }
 
