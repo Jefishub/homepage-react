7a8fadff3d8fcfc8c106d885c0a7aa9e4a88331b
jefishub <jere.manni@gmail.com>
2021-09-29 12:19:13 +0300

    changed profile text

--- src/pages/homepage/Profile/ProfileContent.js
+++ src/pages/homepage/Profile/ProfileContent.js
@@ -2,12 +2,12 @@ import GitHub from '../../../images/GitHub.png'
 import Linkedin from '../../../images/Linkedin.png'
 
 const profile = {
-    header: "Profile:",
-    profileText: "Self-learned Software Developer eager to learn more to excel in whatever I do. With skills in full-stack software development and extensive background in R&D, I find myself a versatile worker who can quickly adopt new technologies, environments, and methods. I have 10 years of working experience in tech fields including 2 years of team leading. Personally, I am an analytical problem solver and a team-player who is into sports, science, and technology. I would love to work with beautiful novel technologies like AR/MR, AI or blockchain which in my mind are going to change the world in the near future. With me, I will also bring joy, happiness and my values that are healthy lifestyle, honesty, compassion, kindness, playfulness, and equality.",
-    links: [
-        {icon: <a href="https://www.linkedin.com/in/jeremanni"><img height="26" src={Linkedin} alt="Linkedin"></img></a>},
-        {icon: <a href="https://www.github.com/jefishub"><img height="26" src={GitHub} alt="GitHub"></img></a>}
-    ]
-  }
+  header: "Profile:",
+  profileText: "Self-learned Software Engineer with almost 1-year of work experience in full-stack web development, extensive background in technology R&D and 2 years of experience in team leading. Analytical problem solver and a team-player who can quickly adopt new technologies and working methods. Would love to work in the future with game-changing technologies like AR/MR, AI or blockchain. Always trying to stay positive bringing with me my values that are healthy lifestyle, compassion, playfulness, and equality.",
+  links: [
+    { icon: <a href="https://www.linkedin.com/in/jeremanni"><img height="26" src={Linkedin} alt="Linkedin"></img></a> },
+    { icon: <a href="https://www.github.com/jefishub"><img height="26" src={GitHub} alt="GitHub"></img></a> }
+  ]
+}
 
 export { profile }
