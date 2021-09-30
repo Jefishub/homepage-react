5add6c7ca9d9d6dfcb8078b7d57be7355f689643
jefishub <jere.manni@gmail.com>
2021-09-30 17:07:45 +0300

    changed navbar home to same style as others

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -8,8 +8,8 @@ function NavBar() {
     return (
         <Navbar bg="dark" variant="dark" sticky="top"> 
             <Container style={{marginLeft:"16px"}}>
-                <Navbar.Brand href="/">Home</Navbar.Brand>
                 <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/work">Work History</Nav.Link>
                     <Nav.Link href="/education">Education</Nav.Link>
                     <Nav.Link href="/skills">Skills</Nav.Link>
